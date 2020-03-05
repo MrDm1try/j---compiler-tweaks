47134d126e6b163fe5a0ad5670b4cd1ee6f224e1
Alexander Birch <alexisbirch@gmail.com>
2020-03-05 11:15:09 +0100

    Added step 2 test files
    From the provided example tests

--- tests/pass/Operators.java
+++ tests/pass/Operators.java
@@ -1,9 +1,14 @@
+import java.lang.System;
+
 public class Operators {
     public static void main(String[] args) {
-        int x = 100;
-        x -= 1;
-        x %= 7;
-        boolean y = x >= 10 || False;
-        int z = y ? 2 : 0;
-    } 
+        System.out.println(true && false || true);
+        int x = 42;
+        x -= 2;
+        x *= 2;
+        x /= 10;
+        x %= 3;
+        System.out.println(x++);
+        System.out.println(--x);
+    }
 }
