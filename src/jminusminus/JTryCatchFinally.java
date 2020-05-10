d79115ff87fe1dcee49ef6127bfbc7e0008d22bf
Dmitry <rachkovskydmitry@gmail.com>
2020-05-10 21:06:30 +0200

    Codegen for try-catch-finally

--- src/jminusminus/JTryCatchFinally.java
+++ src/jminusminus/JTryCatchFinally.java
@@ -22,6 +22,10 @@ class JTryCatchFinallyStatement extends JStatement {
 
     /** Finally block. */
     private JStatement finallyBlock;
+    
+    /** Offsets for storing exception types in stack */
+    private int offset;
+    private int finallyOffset;
 
     /**
      * Construct an AST node for a try-statement given its line number, the
@@ -54,18 +58,18 @@ class JTryCatchFinallyStatement extends JStatement {
      */
 
     public JTryCatchFinallyStatement analyze(Context context) {
+        this.offset = ((LocalContext) context).nextOffset();
+        this.finallyOffset = ((LocalContext) context).nextOffset();
         tryBlock = (JStatement) tryBlock.analyze(context);
         for (int i = 0; i < catchBlocks.size(); i++)
         	catchBlocks.set(i, (JStatement) catchBlocks.get(i).analyze(context));
-        finallyBlock = (JStatement) finallyBlock.analyze(context);
-        
-
-        for (JFormalParameter catchException : exceptionsToCatch) {
-        	catchException.setType(catchException.type().resolve(context));
+        if (finallyBlock != null) {
+			finallyBlock = (JStatement) finallyBlock.analyze(context);
         }
-        
-        for (JFormalParameter catchException : exceptionsToCatch) {
-            if (!Type.EXCEPTION.isJavaAssignableFrom(catchException.type())) {
+
+        for (int i = 0; i < exceptionsToCatch.size(); i++) {
+        	exceptionsToCatch.get(i).setType(exceptionsToCatch.get(i).type().resolve(context));
+            if (!Type.EXCEPTION.isJavaAssignableFrom(exceptionsToCatch.get(i).type())) {
     			JAST.compilationUnit.reportSemanticError(line(), "Catch expression is not an exception.");
             }
         }
@@ -82,7 +86,121 @@ class JTryCatchFinallyStatement extends JStatement {
      */
 
     public void codegen(CLEmitter output) {
-        //todo
+        String endLabel = output.createLabel();
+        String tryStart = output.createLabel();
+        String tryEnd = output.createLabel();
+		String finallyStart = output.createLabel();
+		String finallyEnd = output.createLabel();
+
+		// to label start of every catch clause
+        ArrayList<String> startCatchLabels = new ArrayList<String>();
+		// to label end of every catch clause
+        ArrayList<String> endCatchLabels = new ArrayList<String>();
+        
+        /* Generating instructions */
+        
+		output.addLabel(tryStart);
+        tryBlock.codegen(output);
+		output.addLabel(tryEnd);
+        if (finallyBlock != null) {
+			finallyBlock.codegen(output);
+        }
+		output.addBranchInstruction(GOTO, endLabel);
+
+        for(int i = 0; i < catchBlocks.size(); i++) {
+        	startCatchLabels.add(output.createLabel());
+        	endCatchLabels.add(output.createLabel());
+
+			output.addLabel(startCatchLabels.get(i));
+
+			switch (this.offset) {
+				case 0:
+					output.addNoArgInstruction(ASTORE_0);
+					break;
+				case 1:
+					output.addNoArgInstruction(ASTORE_1);
+					break;
+				case 2:
+					output.addNoArgInstruction(ASTORE_2);
+					break;
+				case 3:
+					output.addNoArgInstruction(ASTORE_3);
+					break;
+				default:
+					output.addOneArgInstruction(ASTORE, this.offset);
+					break;
+			}
+			catchBlocks.get(i).codegen(output);
+			if (finallyBlock != null) {
+				finallyBlock.codegen(output);
+			}
+			// GOTO instruction after the last catch block can be skipped
+			// unless there is a finally block
+			if ((finallyBlock != null) || (i < catchBlocks.size() - 1))
+				output.addBranchInstruction(GOTO, endLabel);
+
+			output.addLabel(endCatchLabels.get(i));
+        }
+
+		if (finallyBlock != null) {
+			output.addLabel(finallyStart);
+			switch (this.finallyOffset) {
+				case 0:
+					output.addNoArgInstruction(ASTORE_0);
+					break;
+				case 1:
+					output.addNoArgInstruction(ASTORE_1);
+					break;
+				case 2:
+					output.addNoArgInstruction(ASTORE_2);
+					break;
+				case 3:
+					output.addNoArgInstruction(ASTORE_3);
+					break;
+				default:
+					output.addOneArgInstruction(ASTORE, this.finallyOffset);
+					break;
+			}
+			finallyBlock.codegen(output);
+			switch (this.finallyOffset) {
+				case 0:
+					output.addNoArgInstruction(ALOAD_0);
+					break;
+				case 1:
+					output.addNoArgInstruction(ALOAD_1);
+					break;
+				case 2:
+					output.addNoArgInstruction(ALOAD_2);
+					break;
+				case 3:
+					output.addNoArgInstruction(ALOAD_3);
+					break;
+				default:
+					output.addOneArgInstruction(ALOAD, this.finallyOffset);
+					break;
+			}
+			output.addNoArgInstruction(ATHROW);
+			output.addLabel(finallyEnd);
+		}
+
+		output.addLabel(endLabel);
+
+        /* Filling in the exception table */
+        
+		// Directing exceptions from try block to designated catch blocks 
+        for(int i = 0; i < exceptionsToCatch.size(); i++) {
+        	output.addExceptionHandler(tryStart, tryEnd, startCatchLabels.get(i), exceptionsToCatch.get(i).type().jvmName());
+        }
+
+		// Directing exceptions from try block, all catch blocks and finally block to finally block 
+        // to ensure that if an exception is raised in any of these, the code for the finally block is executed
+		if (finallyBlock != null) {
+        	output.addExceptionHandler(tryStart, tryEnd, finallyStart, null);
+			for(int i = 0; i < startCatchLabels.size(); i++) {
+				output.addExceptionHandler(startCatchLabels.get(i), endCatchLabels.get(i), finallyStart, null);			
+			}
+        	output.addExceptionHandler(finallyStart, finallyEnd, finallyStart, null);
+		}
     }
 
     /**
--- tests/pass/ExceptionHandlers.java
+++ tests/pass/ExceptionHandlers.java
@@ -1,4 +1,5 @@
 import java.lang.Exception;
+import java.lang.System;
 
 class ExceptionOne extends Exception {}
 class ExceptionTwo extends Exception {}
@@ -8,16 +9,21 @@ public class ExceptionHandlers {
 		
 	}
 	
+    private static void g1() { System.out.println("g1");}
+    private static void g2() { System.out.println("g2");}
+    private static void g3() { System.out.println("not to forget");}
+
     private static void f() throws ExceptionOne, ExceptionTwo {
         throw new ExceptionOne();
+//        throw new ExceptionTwo();
     }
     
     public static void main(String[] args) {
         try {
-            f();
+        	f();
         }
-        catch (ExceptionOne e1) { ; }
-        catch (ExceptionTwo e2) { ; }
-        finally { ; }
+        catch (ExceptionOne e1) { g1(); }
+        catch (ExceptionTwo e2) { g2(); }
+        finally { g3(); }
     }
 }
